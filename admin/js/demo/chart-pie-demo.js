ccb30e4c0e0e8fdcb2a13614c92e88ce44352210
brahim-kellou <fb_kellou@esi.dz>
2019-09-21 02:48:11 +0100

    first commit

---
+++ admin/js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Forestières", "Alimentation et Agriculture", "Marines", "Faune Sauvage et Domestique"],
+    datasets: [{
+      data: [36, 17, 24, 23],
+      backgroundColor: ['#91c83e', '#16a085', '#4cbbcd', '#e67e22'],
+      hoverBackgroundColor: ['#91c83e', '#16a085', '#4cbbcd', '#e67e22'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
